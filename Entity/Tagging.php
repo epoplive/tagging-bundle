aa7f9b6b586bfc67f89aedd665658017c818792e
Mike Klimin <klinkin@gmail.com>
2016-11-14 13:37:45 +0300

    Явно поля в моделе

--- Entity/Tagging.php
+++ Entity/Tagging.php
@@ -7,25 +7,50 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\Mapping\UniqueConstraint;
 
 /**
- * Fogs\TaggingBundle\Entity\Tagging
- *
- * @ORM\Table(uniqueConstraints={@UniqueConstraint(name="tagging_idx", columns={"tag_id", "resource_type", "resource_id"})})
+ * @ORM\Table(name="tagging",
+ * 		uniqueConstraints={@UniqueConstraint(name="tagging_idx", columns={"tag_id", "resource_type", "resource_id"})}
+ * )
  * @ORM\Entity
  */
 class Tagging extends BaseTagging
 {
-    /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
+	/**
+	 * @var integer $id
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
     protected $id;
 
     /**
-     * @ORM\ManyToOne(targetEntity="Tag", inversedBy="tagging")
+     * @ORM\ManyToOne(targetEntity="Fogs\TaggingBundle\Entity\Tag", inversedBy="tagging"))
      * @ORM\JoinColumn(name="tag_id", referencedColumnName="id")
      **/
     protected $tag;
+
+    /**
+     * @var string
+     * @ORM\Column(name="resource_type", type="string", length=50)
+     */
+    protected $resourceType;
+
+    /**
+     * @var string
+     * @ORM\Column(name="resource_id", type="string", length=50)
+     */
+    protected $resourceId;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    protected $updatedAt;
+
 }
