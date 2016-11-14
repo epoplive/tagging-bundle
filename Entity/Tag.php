ce12932d48540114278dcc4daadea0936a4053d6
Mike Klimin <klinkin@gmail.com>
2016-11-14 13:28:32 +0300

    Явно указываем поля БД в модели Tag

--- Entity/Tag.php
+++ Entity/Tag.php
@@ -1,29 +1,52 @@
-<?php 
+<?php
 
 namespace Fogs\TaggingBundle\Entity;
 
 use \FPN\TagBundle\Entity\Tag as BaseTag;
 use Doctrine\ORM\Mapping as ORM;
 
+
 /**
- * Fogs\TaggingBundle\Entity\Tag
- *
- * @ORM\Table()
+ * @ORM\Table(name="tags")
  * @ORM\Entity
  */
 class Tag extends BaseTag
 {
-    /**
-     * @var integer $id
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
+	/**
+	 * @var integer $id
+	 *
+	 * @ORM\Column(name="id", type="integer")
+	 * @ORM\Id
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
     protected $id;
 
     /**
-     * @ORM\OneToMany(targetEntity="Tagging", mappedBy="tag", fetch="EAGER")
+     * @var string
+	 * @ORM\Column(name="name", type="string", length=50)
+     */
+    protected $name;
+
+    /**
+     * @var string
+	 * @ORM\Column(name="slug", type="string", length=50)
+     */
+    protected $slug;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="updated_at", type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Fogs\TaggingBundle\Entity\Tagging", mappedBy="tag", fetch="EAGER")
      **/
     protected $tagging;
 }
